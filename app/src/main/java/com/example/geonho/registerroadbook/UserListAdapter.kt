44f234488fce59ce933869df5d121e8f83cc6afe
GeoN5 <sgepyh2916@naver.com>
2018-09-27 13:46:09 +0900

    cursorAdapter implement

--- app/src/main/java/com/example/geonho/registerroadbook/UserListAdapter.kt
+++ app/src/main/java/com/example/geonho/registerroadbook/UserListAdapter.kt
@@ -2,7 +2,10 @@ package com.example.geonho.registerroadbook
 
 import android.content.Context
 import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,16 +25,32 @@ class UserListAdapter(context: Context,cursor: Cursor?):CursorAdapter(context,cu
     override fun newView(context: Context?, cursor: Cursor?, parent: ViewGroup?): View {
         val inflater = context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val mainView = inflater.inflate(R.layout.layour_user_list,parent,false)
-        var holder:ViewHolder = ViewHolder(mainView.findViewById(R.id.profile) as ImageView,)
-
+        var holder:ViewHolder = ViewHolder(mainView.findViewById(R.id.profile) as ImageView,
+                                mainView.findViewById(R.id.name) as TextView,
+                                mainView.findViewById(R.id.tel_num) as TextView,
+                                mainView.findViewById(R.id.del_item) as ImageView)
+        mainView.tag = holder
+        return mainView
     }
 
+    //새로 생성된 뷰가 화면상에 보여질 때 호출되는 함수
     override fun bindView(convertView: View, context: Context, cursor: Cursor) {
-
+        val holder = convertView.tag as ViewHolder
+        holder.name.text = String.format("%s (%d)",cursor.getString(1),cursor.getInt(2))
+        holder.tel.text = cursor.getString(3)
+        val picture:Drawable = getPicture(cursor.getString(4))?:context.getDrawable(android.R.drawable.ic_menu_gallery)
+        holder.pic.background = picture
+        //save cursor id
+        holder.del.tag = cursor.getLong(0)
     }
 
     private fun getPicture(path:String): Drawable?{
+        val img_id = path.toLong()
+        if(img_id==0L) return null
 
+        val bitmap: Bitmap = MediaStore.Images.Thumbnails.getThumbnail(mCtx.contentResolver,img_id,MediaStore.Images.Thumbnails.MICRO_KIND,null)
+        bitmap?:return null
+        return BitmapDrawable(mCtx.resources,bitmap)
     }
 
 }
